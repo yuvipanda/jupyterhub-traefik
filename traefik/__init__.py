279c99a9007fcca4c5032c869e0a78501bd44154
yuvipanda <yuvipanda@gmail.com>
2017-06-29 14:56:15 -0700

    Add support for get_all_routes
    - Also only return routes that were added by JupyterHub,
      rather than all routes.

--- traefik/__init__.py
+++ traefik/__init__.py
@@ -1,4 +1,4 @@
-from escapism import escape
+from escapism import escape, unescape
 import json
 import etcd
 import os
@@ -16,13 +16,17 @@ class Traefik(Proxy):
         return os.path.join(self.prefix, *args)
 
     @gen.coroutine
-    def add_route(self, routespec, backend, data=None):
+    def add_route(self, routespec, backend, data):
         if routespec.startswith('/'):
             # url only spec
             host = None
             url_prefix = routespec
         else:
             host, url_prefix = routespec.split('/', 1)
+
+        if data is None:
+            data = {}
+        data.update({'jupyterhub-route': True})
 
         name = escape(routespec, escape_char='-')
         self.log.info(name)
@@ -38,9 +42,8 @@ class Traefik(Proxy):
             self.client.write(self._k(
                 'frontends', name, 'routes', 'host', 'rule'), 'Host:{}'.format(host))
 
-        if data:
-            self.client.write(
-                self._k('frontends', name, 'extra'), json.dumps(data))
+        self.client.write(
+            self._k('frontends', name, 'extra'), json.dumps(data))
 
     @gen.coroutine
     def get_route(self, routespec):
@@ -53,7 +56,37 @@ class Traefik(Proxy):
 
 
     @gen.coroutine
+    def get_all_routes(self):
+        routes = {}
+        items = self.client.read(self.prefix, recursive=True)
+        for item in items.children:
+            key = item.key[len(self.prefix):]
+            if key.startswith("/backends"):
+                routespec = unescape(key.split("/")[1], escape_char='-')
+                target = item.value
+
+                if routespec not in routes:
+                    routes[routespec] = {}
+                routes[routespec].update({
+                    'routespec': routespec,
+                    'target': target
+                })
+            elif key.startswith("/frontends"):
+                parts = key.split("/")
+                if parts[-1] == 'extra':
+                    routespec = unescape(parts[1], escape_char='-')
+                    if routespec not in routes:
+                        routes[routespec] = {}
+                    routes[routespec].update({
+                        'routespec': routespec,
+                        'data': json.loads(item.value)
+                    })
+            else:
+                continue
+        return {k: v for k, v in routes.items if v['data'].get('jupyterhub-route', False)}
+
+    @gen.coroutine
     def delete_route(self, routespec):
-        name = escape(routespec)
+        name = escape(routespec, escape_char='-')
         self.client.delete(os.path.join(self.prefix, 'backends', name), recursive=True)
         self.client.delete(os.path.join(self.prefix, 'frontends', name), recursive=True)
